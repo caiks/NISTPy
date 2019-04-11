351e13d428413ff5a6bbfd9dae9111d104eb4ff0
Cliff McCartney <caiks@greenlake.co.uk>
2019-04-11 16:48:47 +0100

    Add files via upload

--- NISTDev.py
+++ NISTDev.py
@@ -76,6 +76,37 @@ def nistTrainBucketedIO(d):
 def nistTrainIO():
     return nistTrainBucketedIO(256)
 
+# nistTrainBucketedRectangleRandomIO :: Int -> Int -> Int -> Int -> IO (System, HistoryRepa)
+
+def nistTrainBucketedRectangleRandomIO(d,bx,by,s):
+    def lluu(ll):
+        return listsSystem([(v,sset(ww)) for (v,ww) in ll])
+    uvals = systemsVarsSetValue
+    f = gzip.open('train-images-idx3-ubyte.gz','rb')
+    _, z, rows, cols = struct.unpack(">IIII", f.read(16))
+    p = np.frombuffer(f.read(),np.dtype('ubyte')).astype(dtype='int32').reshape([z,rows,cols])
+    r = p * d // 256
+    np.random.seed(s)
+    vrx = np.random.randint(0,rows-bx,z)
+    vry = np.random.randint(0,cols-by,z)
+    r = np.concatenate([r[i:i+1,vrx[i]:vrx[i]+bx,vry[i]:vry[i]+by] for i in range(z)]).reshape([z,bx*by])
+    f = gzip.open('train-labels-idx1-ubyte.gz','rb')
+    _ = f.read(8)
+    l = np.frombuffer(f.read(),np.dtype('ubyte')).astype(dtype='int32').reshape([z,1])
+    h = np.concatenate((np.transpose(l),np.transpose(r)))
+    uu = lluu([(VarStr("digit"),[ValInt(i) for i in range(9)])] + [(VarPair((VarInt(x), VarInt(y))), [ValInt(i) for i in range(d)]) for x in range(1,bx+1) for y in range(1,by+1)])
+    vv = list(uvars(uu))
+    mvv = sdict([(v,i) for (i,v) in enumerate(vv)])
+    mm = sdict([(v,sdict([(w,i) for (i,w) in enumerate(uvals(uu,v))])) for v in vv])
+    sh = tuple([len(mm[v]) for v in vv])
+    hr = (vv,mvv,sh,h)
+    return (uu,hr)
+
+# nistTrainBucketedRegionRandomIO :: Int -> Int -> Int -> IO (System, HistoryRepa)
+
+def nistTrainBucketedRegionRandomIO(d,b,s):
+    return nistTrainBucketedRectangleRandomIO(d,b,b,s)
+
 # nistTestBucketedIO :: Int -> IO (System, HistoryRepa)
 
 def nistTestBucketedIO(d):
