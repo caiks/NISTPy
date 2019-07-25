264d054414d226d92d6ff5efdaf7fb7587ff87b6
Cliff McCartney <caiks@greenlake.co.uk>
2019-07-25 14:24:55 +0100

    Add files via upload

---
+++ NIST_test.py
@@ -0,0 +1,72 @@
+﻿#!/usr/bin/env python3
+
+from NISTDev import *
+from timeit import default_timer as timer
+from sys import stdout, argv
+
+def amax(aa):
+    ll = aall(norm(trim(aa)))
+    ll.sort(key = lambda x: x[1], reverse = True)
+    return llaa(ll[:1])
+
+if __name__ == '__main__':
+    t1 = timer()
+    print(">>>")
+    stdout.flush()
+
+    model = argv[1]
+
+    print("model: %s" % model)
+    stdout.flush()
+
+    (uu,hrtr) = nistTrainBucketedIO(2)
+
+    hr = hrev([i for i in range(hrsize(hrtr)) if i % 8 == 0],hrtr)
+
+    print("train size: %d" % hrsize(hr))
+    stdout.flush()
+
+    digit = VarStr("digit")
+    vv = uvars(uu)
+    vvl = sset([digit])
+    vvk = vv - vvl
+
+    df1 = dfIO(model + '.json')
+
+    print("model cardinality: %d" % len(fvars(dfff(df1))))
+    stdout.flush()
+
+    uu1 = uunion(uu,fsys(dfff(df1)))
+
+    ff1 = dfnul(uu1,df1,9)
+
+    print("nullable fud cardinality: %d" % len(fvars(ff1)))
+    print("nullable fud derived cardinality: %d" % len(fder(ff1)))
+    print("nullable fud underlying cardinality: %d" % len(fund(ff1)))
+    stdout.flush()
+
+    uu1 = uunion(uu,fsys(ff1))
+
+    hr1 = hrfmul(uu1,ff1,hr)
+
+    print("ff label ent: %.16f" % hrlent(uu1,hr1,fder(ff1),vvl))
+    stdout.flush()
+
+    (uu,hrte) = nistTestBucketedIO(2)
+
+    hrq = hrev([i for i in range(hrsize(hrte)) if i % 10 == 0],hrte)
+
+    print("test size: %d" % hrsize(hrq))
+    stdout.flush()
+    
+    hrq1 = hrfmul(uu1,ff1,hrq)
+
+    print("effective size: %d" % int(size(mul(hhaa(hrhh(uu1,hrhrred(hrq1,fder(ff1)))),eff(hhaa(hrhh(uu1,hrhrred(hr1,fder(ff1)))))))))
+    stdout.flush()
+
+    print("matches: %d" % len([rr for (_,ss) in hhll(hrhh(uu1,hrhrred(hrq1,fder(ff1)|vvl))) for qq in [single(ss,1)] for rr in [araa(uu1,hrred(hrhrsel(hr1,hhhr(uu1,aahh(red(qq,fder(ff1))))),vvl))] if size(rr) > 0 and size(mul(amax(rr),red(qq,vvl))) > 0]))
+    stdout.flush()
+
+    t2 = timer()
+    print("<<< done %.3fs" % (t2-t1))
+    stdout.flush()
